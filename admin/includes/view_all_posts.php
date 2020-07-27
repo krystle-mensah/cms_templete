f00700378fa165f1002edfa6440857123f11407e
Krystle Mensah <krystlemensah@Krystles-MBP.cust.communityfibre.co.uk>
2020-07-27 15:58:19 +0100

    Relating Categories to posts and Displaying it

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -39,21 +39,35 @@
     
     //display 
     echo "<tr>";    
-      echo "<td>$post_id</td>";
-      echo "<td> $post_author</td>";
-      echo "<td> $post_title</td>";
-      echo "<td>$post_category_id</td>";
-      echo "<td>$post_status</td>";
-      echo "<td><img width='100' src='../images/$post_image' alt='image'></td>";
-      echo "<td>$post_tags</td>";
-      echo "<td>$post_comment_count</td>";
-      echo "<td>$post_date</td>";
-      //passing the page and the post id. 
-      echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
-      //send it to the url
-      echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
+    echo "<td>$post_id</td>";
+    echo "<td> $post_author</td>";
+    echo "<td> $post_title</td>";
+      
+      // select ALL from the table where [column name] variable is equal to this [column name] variable.
+      $request_to = "SELECT * FROM categories WHERE cat_id = {$post_category_id} ";
+                          
+      // function to send query into the database. 
+      $select_categories_id = mysqli_query($connection,$request_to);
+       
+      // while the condition is true fetch the row representing the array from ($variable - see above)
+      while($row = mysqli_fetch_array($select_categories_id)) {
+        // Then assign the array to a variable
+        $cat_id = $row['cat_id'];
+        $cat_title = $row['cat_title'];
+        // display the cat title 
+        echo "<td>{$cat_title}</td>";
+      }
+      
+    echo "<td>$post_status</td>";
+    echo "<td><img width='100' src='../images/$post_image' alt='image'></td>";
+    echo "<td>$post_tags</td>";
+    echo "<td>$post_comment_count</td>";
+    echo "<td>$post_date</td>";
+    //passing the page and the post id. 
+    echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
+    //send it to the url
+    echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
     echo "</tr>";
-    
   }
 
   ?>
