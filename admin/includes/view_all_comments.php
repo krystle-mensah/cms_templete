10a9fb35ba00bdac078544d060aa6c10828edfe4
Krystle Mensah <krystlemensah@Krystles-MBP.cust.communityfibre.co.uk>
2020-08-02 14:09:47 +0100

    comments setup

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -4,13 +4,14 @@
     <tr>
       <th>Id</th>
       <th>Author</th>
-      <th>Title</th>
-      <th>Category</th>
-      <th>Status</th>
-      <th>Image</th>
-      <th>Tags</th>
       <th>Comments</th>
-      <th>Date</th>      
+      <th>Email</th>
+      <th>Status</th>
+      <th>In Response to</th>
+      <th>Date</th>
+      <th>Approve</th>
+      <th>Unapprove</th>
+      <th>Delete</th>            
     <!-- </tr> -->
   </thead>
   <tbody>
@@ -18,51 +19,50 @@
   <?php 
 
   // select all from the posts table.
-  $query = "SELECT * FROM posts";
+  $query = "SELECT * FROM comments";
 
   // mysqli_query function sends in the above query and connection. 
-  $select_posts = mysqli_query($connection,$query);
+  $select_comments = mysqli_query($connection,$query);
 
   //condition is true fetch the row representing the array from ($variable)
-  while($row = mysqli_fetch_array($select_posts)) {
+  while($row = mysqli_fetch_array($select_comments)) {
 
     // values we bring back and assign to variable
-    $post_id = $row['post_id'];
-    $post_author = $row['post_author'];
-    $post_title = $row['post_title'];
-    $post_category_id = $row['post_category_id'];
-    $post_status = $row['post_status'];
-    $post_image = $row['post_image'];
-    $post_tags = $row['post_tags'];
-    $post_comment_count = $row['post_comment_count'];
-    $post_date = $row['post_date'];
+    $comment_id = $row['comment_id'];
+    $comment_post_id = $row['comment_post_id'];
+    $comment_author = $row['comment_author'];
+    $comment_content = $row['comment_content'];
+    $comment_email = $row['comment_email'];
+    $comment_status = $row['comment_status'];
+    $comment_date = $row['comment_date'];
     
     //display 
     echo "<tr>";    
-    echo "<td>$post_id</td>";
-    echo "<td> $post_author</td>";
-    echo "<td> $post_title</td>";
+    echo "<td>$comment_id</td>";
+    echo "<td>$comment_author</td>";
+    echo "<td>$comment_content</td>";
       
-      // select ALL from the table where [column name] variable is equal to this [column name] variable.
-      $request_to = "SELECT * FROM categories WHERE cat_id = {$post_category_id} ";
+      // // select ALL from the table where [column name] variable is equal to this [column name] variable.
+      // $request_to = "SELECT * FROM categories WHERE cat_id = {$post_category_id} ";
                           
-      // function to send query into the database. 
-      $select_categories_id = mysqli_query($connection,$request_to);
+      // // function to send query into the database. 
+      // $select_categories_id = mysqli_query($connection,$request_to);
        
-      // while the condition is true fetch the row representing the array from ($variable - see above)
-      while($row = mysqli_fetch_array($select_categories_id)) {
-        // Then assign the array to a variable
-        $cat_id = $row['cat_id'];
-        $cat_title = $row['cat_title'];
-        // display the cat title 
-        echo "<td>{$cat_title}</td>";
-      }
+      // // while the condition is true fetch the row representing the array from ($variable - see above)
+      // while($row = mysqli_fetch_array($select_categories_id)) {
+      //   // Then assign the array to a variable
+      //   $cat_id = $row['cat_id'];
+      //   $cat_title = $row['cat_title'];
+      //   // display the cat title 
+      //   echo "<td>{$cat_title}</td>";
+      // }
       
-    echo "<td>$post_status</td>";
-    echo "<td><img width='100' src='../images/$post_image' alt='image'></td>";
-    echo "<td>$post_tags</td>";
-    echo "<td>$post_comment_count</td>";
-    echo "<td>$post_date</td>";
+    echo "<td>$comment_status</td>";
+    echo "<td>$comment_date</td>";
+    //passing the page and the post id. 
+    echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Approve</a></td>";
+    //send it to the url
+    echo "<td><a href='posts.php?delete={$post_id}'>Unapprove</a></td>";
     //passing the page and the post id. 
     echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
     //send it to the url
@@ -77,18 +77,18 @@
 
 <?php
 
-// if this is set
-if(isset($_GET['delete'])){
+// // if this is set
+// if(isset($_GET['delete'])){
 
-  // then convert this into the $the_post_id variable
-  $the_post_id = $_GET['delete']; 
+//   // then convert this into the $the_post_id variable
+//   $the_post_id = $_GET['delete']; 
 
-  // delete
-  $query = "DELETE FROM posts WHERE post_id = {$the_post_id} ";
+//   // delete
+//   $query = "DELETE FROM posts WHERE post_id = {$the_post_id} ";
 
-  // function performs a query against a database to send in. 
-  $delete_query = mysqli_query($connection,$query);
+//   // function performs a query against a database to send in. 
+//   $delete_query = mysqli_query($connection,$query);
 
-}
+// }
 
 ?>
