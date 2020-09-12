96883cd68e6545e3828d708b864b1ab832b57828
Krystle Mensah <krystlemensah@Krystles-MBP.cust.communityfibre.co.uk>
2020-09-12 20:21:16 +0100

    url for edit botton fix

--- includes/navigation.php
+++ includes/navigation.php
@@ -50,7 +50,7 @@
 								
 							$the_post_id = $_GET['p_id'];
 						
-						echo "<li><a href='/cms/admin/posts.php?source=edit_post&p_id={$the_post_id}'>Edit Post</a></li>";
+						echo "<li><a href='/cms_templete/admin/posts.php?source=edit_post&p_id={$the_post_id}'>Edit Post</a></li>";
 						
 						}
 				
