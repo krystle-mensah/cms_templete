89de0b00e7094399dd4dbb80035c59a8ed0fa9b7
Krystle Mensah <krystlemensah@Krystles-MBP.cust.communityfibre.co.uk>
2020-09-13 22:51:43 +0100

    testing

--- includes/db.php
+++ includes/db.php
@@ -11,25 +11,19 @@
 
 // SECURE CONNECTION
 
+$serverName = "localhost";
+$username = "root";
+$password = "";
+$databaseName = "cms"; 
+
 // db array assigned to value's
-$db['db_host'] = "localhost";
-$db['db_user'] = "root";
-$db['db_pass'] = "";
-$db['db_name'] = "cms";
-
-//echo $db['db_host'];//localhost
-
-
-//echo "\n";
+$db['db_host'] = $serverName;
+$db['db_user'] = $username;
+$db['db_pass'] = $password;
+$db['db_name'] = $databaseName;
 
 // foreach array use key name and loop through each value.
 foreach($db as $key => $value){
-  // we take the whole array as the key name
-  
-  //print_r($db);// the whole array looped
-  //echo "\n";
-  //print_r($key);//key name
-  //echo "\n";
 
   define(strtoupper($key),$value);
 
@@ -38,8 +32,10 @@ foreach($db as $key => $value){
 //assign variable to open a new connection to server with contants
 $connection = mysqli_connect(DB_HOST, DB_USER,DB_PASS,DB_NAME);
 
-// // if connection is ture
-// if($connection) {
-//   // display this in the browser else do nothing. 
-//   echo "We are connected";
-// }
+// if connection is ture
+if($connection) {
+  // display this in the browser else do nothing. 
+  echo "We are connected";
+}
+
+
